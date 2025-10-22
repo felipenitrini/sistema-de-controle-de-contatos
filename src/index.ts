c7e4cd68088874b798ac2cb01dab07df1ed9e007
felipe nitrini <felipe.jacob@etec.sp.gov.br>
2025-10-22 00:14:26 -0300

    atualizacao do site

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,15 @@ app.set('views', './src/views');
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
-    res.render('index', { message: 'Hello' });
+    res.redirect("/login")
 });
+
+
 
 app.get('/login', function (req, res) {
     res.render('login', { message: 'Hello' });
 });
+
 
 app.listen(3333, () => {
     console.log('Servidor rodando no endereço http://localhost:3333');
